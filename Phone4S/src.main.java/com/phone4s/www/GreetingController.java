508e85305edc30d320521e11a108351f776b3990
CLOUD <CLOUD@10.0.85.86>
2014-10-26 22:33:23 +0800

    建立架构

---
+++ Phone4S/src.main.java/com/phone4s/www/GreetingController.java
@@ -0,0 +1,25 @@
+package com.phone4s.www;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+	
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        ApplicationContext act = new FileSystemXmlApplicationContext("C:/Users/CLOUD/git/Phone4S/Phone4S/WebContent/WEB-INF/applicationContext.xml");
+        TestJDBC testJDBC = (TestJDBC) act.getBean("testJDBC");
+        testJDBC.testJDBC();
+        
+        return "test";
+    }
+
+}
---
+++ Phone4S/src.main.java/com/phone4s/www/TestJDBC.java
@@ -0,0 +1,60 @@
+package com.phone4s.www;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class TestJDBC {
+	
+	public DriverManagerDataSource dataSource = null;
+
+	
+	public DriverManagerDataSource getDataSource() {
+		return dataSource;
+	}
+
+
+	public void setDataSource(DriverManagerDataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	
+	
+	public String testJDBC() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(this.dataSource);
+		
+		
+		String[] names = "John Woo;Jeff Dean;Josh Bloch;Josh Long".split(";");
+		for (String fullname : names) {
+			String[] name = fullname.split(" ");
+			System.out.printf("Inserting customer record for %s %s\n", name[0], name[1]);
+			jdbcTemplate.update(
+					"INSERT INTO customers(first_name,last_name) values(?,?)",
+					name[0], name[1]);
+		}
+		
+		System.out.println("Querying for customer records where first_name = 'Josh':");
+		List<Customer> results = jdbcTemplate.query(
+				"select * from customers where first_name = ?", new Object[] { "Josh" },
+				new RowMapper<Customer>() {
+					@Override
+					public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+						return new Customer(rs.getLong("id"), rs.getString("first_name"),
+								rs.getString("last_name"));
+					}
+				});
+		
+		for (Customer customer : results) {
+			System.out.println(customer);
+		}
+		
+		return null;
+	}
+
+
+}
